2c9dfcca433d54e63e6494cee1921c86db148abb
Thiago Costa <thiagoocdiniz@gmail.com>
2025-01-23 17:26:32 -0300

    criando as views

--- wallets/views.py
+++ wallets/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .api.v1.transaction_serializer import TransactionSerializer
+from .api.v1.user_serializer import UserSerializer
+from .api.v1.wallet_serializer import WalletSerializer
+from .models import Transaction, User, Wallet
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class WalletViewSet(viewsets.ModelViewSet):
+    queryset = Wallet.objects.all()
+    serializer_class = WalletSerializer
