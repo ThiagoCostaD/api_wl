d6adfed60f76e73e58a638bc82d3536593239c17
Thiago Costa <thiagoocdiniz@gmail.com>
2025-01-30 11:25:34 -0300

    Criando as URLS

--- wallets/urls.py
+++ wallets/urls.py
@@ -1,41 +1,32 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from .views import TransactionViewSet, UserViewSet, WalletViewSet
+from .views import (CreateUserViewSet, TransactionViewSet, UserViewSet,
+                    WalletViewSet)
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename="user")
+router.register(r'wallets', WalletViewSet, basename="wallet")
+router.register(r'transactions', TransactionViewSet, basename="transaction")
 
 urlpatterns = [
-    # Endpoints para transações
+    path('', include(router.urls)),
+
     path(
-        "transactions/",
-        TransactionViewSet.as_view({"get": "list", "post": "create"}),
-        name="transaction-list-create"
-    ),
-    path(
-        "transactions/<int:pk>/",
-        TransactionViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"}),
-        name="transaction-detail"
+        "users/create/",
+        CreateUserViewSet.as_view({"post": "create"}),
+        name="user-create"
     ),
 
-    # Endpoints para usuários
     path(
-        "users/",
-        UserViewSet.as_view({"get": "list", "post": "create"}),
-        name="user-list-create"
-    ),
-    path(
-        "users/<int:pk>/",
-        UserViewSet.as_view({"get": "retrieve"}),
-        name="user-detail"
+        "wallets/<int:pk>/add_balance/",
+        WalletViewSet.as_view({"post": "add_balance"}),
+        name="wallet-add-balance"
     ),
 
-    # Endpoints para carteiras
     path(
-        "wallets/",
-        WalletViewSet.as_view({"get": "list", "post": "create"}),
-        name="wallet-list-create"
-    ),
-    path(
-        "wallets/<int:pk>/",
-        WalletViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"}),
-        name="wallet-detail"
+        "transactions/transfer/",
+        TransactionViewSet.as_view({"post": "transfer"}),
+        name="transaction-transfer"
     ),
 ]
