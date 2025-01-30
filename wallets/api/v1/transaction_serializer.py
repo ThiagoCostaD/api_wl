666e7b031fd93ea5e47e73779e70fb388c03b9e3
Thiago Costa <thiagoocdiniz@gmail.com>
2025-01-30 11:25:46 -0300

    criando as serializações

--- wallets/api/v1/transaction_serializer.py
+++ wallets/api/v1/transaction_serializer.py
@@ -4,6 +4,16 @@ from wallets.models import Transaction
 
 
 class TransactionSerializer(serializers.ModelSerializer):
+
+    sender_username = serializers.CharField(source="sender.account.username", read_only=True)
+    receiver_username = serializers.CharField(source="receiver.account.username", read_only=True)
+
     class Meta:
         model = Transaction
-        fields = ["id", "sender", "receiver", "timestamp", "amount"]
+        fields = ["id", "sender", "sender_username", "receiver",
+                  "receiver_username", "timestamp", "amount"]
+        extra_kwargs = {
+            "sender": {"read_only": True},
+            "receiver": {"read_only": True},
+            "timestamp": {"read_only": True}
+        }
--- wallets/api/v1/wallet_serializer.py
+++ wallets/api/v1/wallet_serializer.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
-from ...models import Wallet
+from wallets.models import Wallet
 
 
 class WalletSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Wallet
         fields = ["id", "account", "balance"]
+        extra_kwargs = {
+            "account": {"read_only": True}
+        }
+
+
+class WalletBalanceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Wallet
+        fields = ["id", "balance"]
